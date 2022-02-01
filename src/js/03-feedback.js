e9324e712386df7e07557ca49ac73ebe17e416d8
Vasyl-Gumeniuk <vasyl.gumeniuk@gmail.com>
2022-02-01 14:50:26 +0200

    Update 03-feedback.js
    Update-03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,10 +6,7 @@ const inputRefs = {
     textarea: document.querySelector('.feedback-form textarea'),
 }
 const FORM_CURRENT_VALUE = "feedback-form-state";
-const userData = {
-    email: '',
-    message: '',
-};
+const userData = {};
 
 
 form.addEventListener('submit', cleanSubmitForm);
@@ -21,17 +18,18 @@ checkSubmitForm();
 function cleanSubmitForm(e) {
     e.preventDefault();
     e.currentTarget.reset();
-    localStorage.removeItem('FORM_CURRENT_VALUE');
+    localStorage.removeItem("FORM_CURRENT_VALUE");
     console.log(userData);
 }
 
 function inputData(e) {
     userData[e.target.name] = e.target.value;
-    localStorage.setItem('FORM_CURRENT_VALUE', JSON.stringify(userData));
+    localStorage.setItem("FORM_CURRENT_VALUE", JSON.stringify(userData));
+    console.log(userData);
 }
 
 function checkSubmitForm(e) {
-    const savedData = localStorage.getItem('FORM_CURRENT_VALUE');
+    const savedData = localStorage.getItem("FORM_CURRENT_VALUE");
     const parsedSavedData = JSON.parse(savedData);
 
     if (savedData) {
@@ -39,3 +37,25 @@ function checkSubmitForm(e) {
         inputRefs.textarea.value = parsedSavedData.message;
     }
 }
+
+
+
+
+
+
+//         if (savedData) {
+//             checkEmail();
+//             checkMessage();
+//         }
+//     function checkEmail() {
+//         if (parsedSavedData.email !== undefined) {
+//             inputRefs.input.value = parsedSavedData.email;
+//         } return
+//     };
+
+//     function checkMessage() {
+//         if (parsedSavedData.message !== undefined) {
+//             inputRefs.textarea.value = parsedSavedData.message;
+//             } return
+//     };
+// };
